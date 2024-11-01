abd4de5ac41fb6560ef188aeb26ef27c1fe3c9d7
DinhHoangGia-pro <dinhhoanggia@gmail.com>
2024-11-01 14:59:34 +0700

    Add files via upload

--- src/main/java/gdu/__java/API_demo/controller/StudentController.java
+++ src/main/java/gdu/__java/API_demo/controller/StudentController.java
@@ -1,15 +1,59 @@
 package gdu.__java.API_demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import gdu.__java.API_demo.model.MyDB;
+import gdu.__java.API_demo.model.Student;
+
+
 
 @RestController
 public class StudentController {
 	
+	
+	
+	@Autowired
+	MyDB mydb;
+	
 	@GetMapping("/liststudents")
-	public String[] listStudents()
-	{
-	return new String[]{"Nguyễn Văn A", "Nguyễn Văn B", "Trần Thị C"};
+	public List<Student> ListStudents()
+	{				
+		return mydb.findAll();
+		
+	}
+	
+	
+	@PostMapping("/insertStudent")
+	public String insertStudent(@RequestBody Student newstudent)
+	{		
+		mydb.save(newstudent);		
+		return "Thêm sinh viên thành công!";	
+		
+	}
+	
+	@PutMapping("/updateStudent")
+	public String updateStudent(@RequestBody Student student)
+	{	
+		mydb.save(student);		
+		return "Sửa sinh viên thành công!";		
+		
+	}
+	@DeleteMapping("/deleteStudent/{id}")
+	public String deleteStudent(@PathVariable Integer id)
+	{		
+		mydb.deleteById(id);		
+		return "Xóa thành công!";
+			
 	}
 
 }
